634a38fb09d2c831cc6d5ae297e0bc4fc69dbee5
nkc20206082 <79066907+nkc20206082@users.noreply.github.com>
2022-03-14 04:03:11 +0300

    Add files via upload

--- DamageTextController.cs
+++ DamageTextController.cs
@@ -8,14 +8,6 @@ public class DamageTextController : MonoBehaviour
     void Start()
     {
         //GetComponent<TextMesh>().text = "10";
-        this.transform.DOMove(new Vector2(transform.position.x, transform.position.y+1f), 0.6f);
-        StartCoroutine(DestroyObject());
-    }
-
-    //Á‚¦‚é‚Ü‚Å
-    private IEnumerator DestroyObject()
-    {
-        yield return new WaitForSeconds(0.6f);
-        Destroy(this.gameObject);
+        this.transform.DOMove(new Vector2(transform.position.x, transform.position.y+1f), 0.6f).OnComplete(() => { Destroy(this.gameObject); });
     }
 }
